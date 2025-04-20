647a3bcfc84e21f74fe7f83432836cdf33becc5b
restevez <restevez@c1r6p4.42sp.org.br>
2025-04-20 00:05:13 -0300

    so_long - Day Three - v0

--- Makefile
+++ Makefile
@@ -4,14 +4,17 @@ LIBMLX	:= ./lib/MLX42
 LIBFT	:= ./lib/libft
 
 HEADERS	:= -I ./include -I $(LIBMLX)/include -I $(LIBFT)/include
-LIBS	:= $(LIBMLX)/build/libmlx42.a -ldl -lglfw -pthread -lm
+LIBS	:= $(LIBMLX)/build/libmlx42.a -ldl -lglfw -pthread -lm -L$(LIBFT) -lft
 SRCS	:= $(shell find ./src -iname "*.c")
 OBJS	:= ${SRCS:.c=.o}
 
-all: libmlx $(NAME)
+all: libmlx libft $(NAME)
 
 libmlx:
 	@cmake $(LIBMLX) -B $(LIBMLX)/build && make -C $(LIBMLX)/build -j4
+
+libft:
+	@$(MAKE) -C $(LIBFT)
 
 %.o: %.c
 	@$(CC) $(CFLAGS) -o $@ -c $< $(HEADERS) && printf "Compiling: $(notdir $<)"
@@ -22,10 +25,12 @@ $(NAME): $(OBJS)
 clean:
 	@rm -rf $(OBJS)
 	@rm -rf $(LIBMLX)/build
+	@$(MAKE) -C $(LIBFT) clean
 
 fclean: clean
 	@rm -rf $(NAME)
+	@$(MAKE) -C $(LIBFT) fclean
 
 re: clean all
 
-.PHONY: all, clean, fclean, re, libmlx
+.PHONY: all, clean, fclean, re, libmlx, libft
--- include/so_long.h
+++ include/so_long.h
@@ -6,14 +6,26 @@
 /*   By: restevez <restevez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 19:49:09 by restevez          #+#    #+#             */
-/*   Updated: 2025/04/19 02:52:10 by restevez         ###   ########.fr       */
+/*   Updated: 2025/04/20 00:04:38 by restevez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
+# define FLOOR			'0'
+# define WALL			'1'
+# define PLAYER			'P'
+# define COL			'C'
+# define EXIT			'E'
+# define TRUE			1
+# define FALSE			0
+# define TILE_SIZE_MIN	16
+# define TILE_SIZE_MAX	64
+# define RIGHT			1
+# define LEFT 			2
 
-# include "../MLX42/include/MLX42/MLX42.h"
+# include "../lib/MLX42/include/MLX42/MLX42.h"
+# include "../lib/libft/libft.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
@@ -32,16 +44,33 @@ struct s_game
 	int		path;
 };
 
+typedef struct s_validation	t_validation;
+struct s_validation
+{
+	double	delay;
+	int32_t	fd;
+	int32_t	column;
+	int32_t	line;
+	int32_t	move;
+	int16_t	coll;
+	int16_t	offset_x;
+	int16_t	offset_y;
+	int8_t	tile_size;
+	int8_t	player;
+	int8_t	exit;
+	int8_t	render;
+};
+
 typedef struct s_map		t_map;
 struct s_map
 {
-	size_t	height;
-	size_t	width;
-	t_pos	exit;
-	t_pos	*collectibles;
-	t_pos	player;
-	t_pos	*walls;
-	t_pos	*floor;
+	t_map	*next;
+	t_map	*prev;
+	t_map	*up;
+	t_map	*down;
+	char	chr;
+	int8_t	enable;
+	int8_t	active;
 };
 
 typedef struct s_position	t_pos;
@@ -50,5 +79,35 @@ struct s_position
 	size_t	x;
 	size_t	y;
 };
+
+typedef struct s_image		t_image;
+struct s_image
+{
+	mlx_image_t		*bg;
+	mlx_image_t		*floor;
+	mlx_image_t		*wall;
+	mlx_image_t		*player;
+	mlx_image_t		*exit;
+	mlx_image_t		*coll;
+	mlx_texture_t	*texture_bg;
+	mlx_texture_t	*texture_floor;
+	mlx_texture_t	*texture_wall;
+	mlx_texture_t	*texture_player;
+	mlx_texture_t	*texture_exit;
+	mlx_texture_t	*texture_coll;
+};
+
+typedef struct s_canvas		t_canvas;
+struct s_canvas
+{
+	mlx_t			*mlx;
+	mlx_texture_t	*icon;
+	t_image			canvas;
+	t_validation	val_data;
+	t_map			*map;
+	t_map			*player_pos;
+	t_map			*exit_pos;
+};
+
 
 #endif
