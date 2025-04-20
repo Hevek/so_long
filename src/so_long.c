7ef6c19099897c48da75dbd7ef6a174af816001c
restevez <restevez@c1r6p4.42sp.org.br>
2025-04-20 01:28:21 -0300

    so_long - Day Three - v1 - Player.png

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: restevez <restevez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 19:49:15 by restevez          #+#    #+#             */
-/*   Updated: 2025/04/20 00:04:31 by restevez         ###   ########.fr       */
+/*   Updated: 2025/04/20 01:27:06 by restevez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,45 @@
 #include <stdbool.h>
 #include "../include/so_long.h"
 #include "../lib/MLX42/include/MLX42/MLX42.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+#define WIDTH 512
+#define HEIGHT 512
+
+static void error(void)
+{
+	puts(mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+int32_t	main(void)
+{
+	// Start mlx
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Test", true);
+	if (!mlx)
+        error();
+
+	// Try to load the file
+	mlx_texture_t* texture = mlx_load_png("./textures/Player.png");
+	if (!texture)
+        error();
+
+	// Convert texture to a displayable image
+	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
+	if (!img)
+        error();
+
+	// Display the image
+	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
+        error();
+
+	mlx_loop(mlx);
+
+	// Optional, terminate will clean up any leftovers, this is just to demonstrate.
+	mlx_delete_image(mlx, img);
+	mlx_delete_texture(texture);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
